d53977ac7431380c0555eb6ef4cfc93b103f9b05
semtagg <semtagg@yandex.ru>
2023-02-22 17:52:18 +0300

    wip

--- lib/services/services_dlopen.h
+++ lib/services/services_dlopen.h
@@ -3,12 +3,12 @@
 
 #include <glib.h>
 
-G_GNUC_INTERNAL int services__get_dlopen_metadata(const char *type, char **output); //
-G_GNUC_INTERNAL GList *services__list_dlopen_agents(void);
-G_GNUC_INTERNAL bool services__dlopen_agent_exists(const char *agent);
-G_GNUC_INTERNAL int services__dlopen_prepare(svc_action_t *op);
+G_GNUC_INTERNAL int services__get_dlopen_metadata(const char *type, char **output); // ???
+G_GNUC_INTERNAL GList *services__list_dlopen_agents(void);  // +
+G_GNUC_INTERNAL bool services__dlopen_agent_exists(const char *agent); // + +
+G_GNUC_INTERNAL int services__dlopen_prepare(svc_action_t *op); // +
 
 G_GNUC_INTERNAL
-enum ocf_exitcode services__dlopen2ocf(const char *action, int exit_status);
+enum ocf_exitcode services__dlopen2ocf(const char *action, int exit_status); // +
 
 #endif
